5ed906c9fe323142064545d86be0946d07c1b84d
Kirito <nikhilsimontoppo@gmail.com>
2025-05-26 22:40:57 +0530

    Implemented Zoom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { Canvas, Rect } from "fabric";
+import { Canvas, Rect, Textbox } from "fabric";
 import Toolbar from "./components/Toolbar";
 import Sidebar from "./components/Sidebar";
 
@@ -9,14 +9,14 @@ export default function App() {
   const [menuLeft, setMenuLeft] = useState(0);
   const [menuTop, setMenuTop] = useState(0);
   const [fabricCanvas, setFabricCanvas] = useState(null);
-
+  const [currentZoom, setCurrentZoom] = useState(100);
+  const [isDragging, setIsDragging] = useState(false);
 
   useEffect(() => {
-
     // Initialize Fabric.js canvas
     const canvas = new Canvas(canvasRef.current, {
-      width: 500,
-      height: 500,
+      width: 1500,
+      height: 800,
       backgroundColor: "#f0f0f0",
     });
 
@@ -31,6 +31,8 @@ export default function App() {
     canvas.add(rect);
     setFabricCanvas(canvas);
 
+
+//Open custom context menu on right click
     canvas.upperCanvasEl.addEventListener("mousedown", (event) => {
       if (event.button === 2) {
         // Right-click = 2
@@ -52,12 +54,51 @@ export default function App() {
       e.preventDefault()
     );
 
+    // Handle left-click to close the context menu
     canvas.upperCanvasEl.addEventListener("mousedown", (event) => {
       if (event.button === 0) {
         setIsActive(false);
         console.log("Left click");
       }
     });
+
+    //Enable text editing on textbox on click
+    // canvas.upperCanvasEl.addEventListener("mousedown", (event) => {
+    //   const target = event.target;
+    //   if (!target) return;
+    //   const textbox = target._objects?.find((obj) => obj instanceof Textbox);
+   
+    //   if (textbox) {
+    //     textbox.enterEditing();
+    //     textbox.selectAll();
+    //   }
+    // });
+
+
+    //Change cursor to pointer on hover and change of bg of textbox
+    canvas.upperCanvasEl.addEventListener("mouseenter", (event) => {
+      const target=event.target;
+      if(!target) return;
+      const textbox = target._objects?.find((obj) => obj instanceof Textbox);
+      
+      if (textbox) {
+        canvas.defaultCursor = "pointer";
+        textbox.set({backgroundColor:"red"});
+      }
+    })
+
+    //Zooming
+    canvas.upperCanvasEl.addEventListener("wheel", (event) => {
+      const delta = event.deltaY;
+      let zoom = canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      zoom = Math.min(Math.max(zoom, 0.2), 5);
+      canvas.zoomToPoint({ x: event.offsetX, y: event.offsetY }, zoom);
+      setCurrentZoom(zoom * 100);
+      event.preventDefault();
+      event.stopPropagation();
+    });
+
     // const handleResize = () => {
     //   canvas.setWidth(window.innerWidth);
     //   canvas.setHeight(window.innerHeight);
@@ -72,14 +113,13 @@ export default function App() {
     };
   }, []);
 
- const clearCanvas = () => {
+  const clearCanvas = () => {
     if (fabricCanvas) {
       fabricCanvas.getObjects().forEach((obj) => fabricCanvas.remove(obj));
-  }
-  fabricCanvas.renderAll()
-  setIsActive(false);
-
-  }
+    }
+    fabricCanvas.renderAll();
+    setIsActive(false);
+  };
 
   console.log(menuLeft, menuTop);
 
@@ -94,6 +134,12 @@ export default function App() {
   /> */}
       <div className="flex justify-center">
         <canvas className="border-2 border-black" ref={canvasRef} />
+        <div
+          id="zoom-indicator"
+          className="absolute top-2 left-2 bg-white border border-gray-300 text-sm px-3 py-1 rounded shadow-md select-none"
+        >
+          {currentZoom.toFixed(0)}%
+        </div>
       </div>
       {/* <!-- Custom Context Menu --> */}
       <div
@@ -106,7 +152,6 @@ export default function App() {
         style={{
           left: menuLeft,
           top: menuTop,
-     
         }}
       >
         <button
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,4 +1,4 @@
-import { Rect } from 'fabric';
+import { Group, Rect, Textbox } from "fabric";
 
 export default function Toolbar({ canvas }) {
   const addRectangle = () => {
@@ -9,13 +9,71 @@ export default function Toolbar({ canvas }) {
       top: 50,
       width: 100,
       height: 100,
-      fill: 'green',
+      fill: "green",
     });
 
     canvas.add(rect);
     canvas.setActiveObject(rect);
     canvas.requestRenderAll(); // use this instead of canvas.renderAll()
   };
+
+  //function for textbox creation
+  const addTextbox = () => {
+    if (!canvas) return;
+
+    const textbox = new Textbox("Hello World", {
+      left: 100,
+      top: 100,
+      width: 150,
+      fontSize: 20,
+      fontFamily: "Arial",
+      padding: 0,
+      strokeWidth: 10,
+      borderColor: "black",
+      fill:"white",
+      backgroundColor:"black",
+      textAlign: "center",
+      editable: true,
+    });
+   
+    canvas.add(textbox);
+    canvas.setActiveObject(textbox);
+    canvas.requestRenderAll(); // use this instead of canvas.renderAll()
+  }
+
+  //function for Node Creation
+  function createNode(top, left) {
+    if (!canvas) return;
+    const textbox = new Textbox("New Node", {
+      top:top,
+      left:left,
+      width: 120,
+      fontSize: 16,
+      textAlign: "center",
+      editable: true,
+    
+    });
+
+    const padding = 20;
+    const rect = new Rect({
+      top:top,
+      left:left,
+      width: 120,
+      height: 50,
+      fill: "#e0f2fe",
+ 
+    });
+
+    const group = new Group([rect,textbox],{
+      hasBorders: true,
+      hasControls: true,
+    });
+
+    canvas.add(group);
+    canvas.setActiveObject(group);
+    canvas.requestRenderAll(); // use this instead of canvas.renderAll()
+  
+  }
 
   return (
     <div className="fixed top-0 left-1/2 -translate-x-1/2 z-50 bg-white/90 backdrop-blur px-4 py-2 shadow-md border rounded mt-2 flex gap-2">
@@ -25,6 +83,17 @@ export default function Toolbar({ canvas }) {
       >
         ➕ Add Rectangle
       </button>
+      <button
+        onClick={()=>createNode(100, 100)}
+        className="px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700"
+      >
+        Add Node
+      </button>
+      <button
+        onClick={addTextbox}
+        className="px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700">
+        ➕ Add Textbox
+        </button>
     </div>
   );
 }
